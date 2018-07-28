3fc095d8be3ed1949a390b178760823550198215
Brian Schubert <brianm.schubert@gmail.com>
2018-07-28 17:15:13 -0400

    Update default settings module in manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "troop89.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "troop89.settings.prod")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
