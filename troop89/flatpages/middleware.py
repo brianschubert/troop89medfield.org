aba4c4d071729d9223be058f4f35b4be2d62ca02
Brian Schubert <brianm.schubert@gmail.com>
2019-06-07 15:04:01 -0400

    Migrate custom fallback middleware to use the hierarchical flatpage proxy

--- troop89/flatpages/middleware.py
+++ troop89/flatpages/middleware.py
@@ -8,10 +8,10 @@ from django.conf import settings
 from django.http import Http404
 from django.contrib.flatpages.middleware import FlatpageFallbackMiddleware
 
-from .views import csp_exempt_flatpage
+from .views import hierarchical_flatpage
 
 
-class CSPExemptFlatpageFallbackMiddleware(FlatpageFallbackMiddleware):
+class HierarchicalFlatpageFallbackMiddleware(FlatpageFallbackMiddleware):
     """
     Copy of the standard flatpage middleware that modifies that CSP header for
     flatpage views.
@@ -21,7 +21,7 @@ class CSPExemptFlatpageFallbackMiddleware(FlatpageFallbackMiddleware):
         if response.status_code != 404:
             return response  # No need to check for a flatpage for non-404 responses.
         try:
-            return csp_exempt_flatpage(request, request.path_info)
+            return hierarchical_flatpage(request, request.path_info)
         # Return the original response if any errors happened. Because this
         # is a middleware, we can't assume the errors will be caught elsewhere.
         except Http404:
