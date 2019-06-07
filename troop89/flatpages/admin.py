ad4aaefda275a313651e8743e11e6289765d2f00
Brian Schubert <brianm.schubert@gmail.com>
2019-06-07 15:01:31 -0400

    Migrate flat page admin to use hierarchical proxy

--- troop89/flatpages/admin.py
+++ troop89/flatpages/admin.py
@@ -8,9 +8,11 @@ from ckeditor.widgets import CKEditorWidget
 from csp.decorators import csp_update
 from django import forms
 from django.contrib import admin
-from django.contrib.flatpages import admin as flatpage_admin
+from django.contrib.flatpages import admin as flatpage_admin, forms as flatpage_forms
 from django.contrib.flatpages.models import FlatPage
 from django.utils.decorators import method_decorator
+
+from .models import HierarchicalFlatPage
 
 # The CKEditorWidget requires inline css and javascript, which violate the
 # site-wide CSP. This decorator loosens the CSP on the admin pages that load
@@ -21,20 +23,19 @@ _CSP_UPDATE_DECORATOR = csp_update(
 )
 
 
-class FlatPageAdminForm(forms.ModelForm):
+class HierarchicalFlatPageAdminForm(flatpage_forms.FlatpageForm):
     content = forms.CharField(widget=CKEditorWidget())
 
-    class Meta:
-        model = flatpage_admin.FlatPage
-        fields = '__all__'
+    class Meta(flatpage_forms.FlatpageForm.Meta):
+        model = HierarchicalFlatPage
 
 
 @method_decorator(_CSP_UPDATE_DECORATOR, name='add_view')
 @method_decorator(_CSP_UPDATE_DECORATOR, name='change_view')
-class FlatPageAdmin(flatpage_admin.FlatPageAdmin):
-    form = FlatPageAdminForm
+class HierarchicalFlatPageAdmin(flatpage_admin.FlatPageAdmin):
+    form = HierarchicalFlatPageAdminForm
 
 
 # Re-register FlatPageAdmin
 admin.site.unregister(FlatPage)
-admin.site.register(FlatPage, FlatPageAdmin)
+admin.site.register(HierarchicalFlatPage, HierarchicalFlatPageAdmin)
