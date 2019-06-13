b066cf783f358b6f55fee4cae20c0a45aee7a373
Brian Schubert <brianm.schubert@gmail.com>
2019-06-12 21:22:17 -0400

    Update flatpage breadcrumb structured data to serve absolute urls

--- troop89/flatpages/models.py
+++ troop89/flatpages/models.py
@@ -6,7 +6,9 @@
 
 from pathlib import PurePath
 
+from django.conf import settings
 from django.contrib.flatpages.models import FlatPage
+from django.contrib.sites.models import Site
 from django.db import models
 from django.db.models.functions import Length
 from django.db.utils import cached_property
@@ -94,19 +96,24 @@ class HierarchicalFlatPage(FlatPage):
     @property
     def sd_breadcrumb(self) -> dict:
         """Return this page's breadcrumb trail as a structured data dict."""
+        domain = Site.objects.get_current().domain
+        protocol = 'https' if settings.SECURE_SSL_REDIRECT else 'http'
         item_list = []
+
         for position, page in enumerate(reversed(self.parent_pages), start=1):
             item_list.append({
                 "@type": "ListItem",
                 "position": position,
                 "name": page.title,
-                "item": page.url
+                # Absolute url construction as recommended in the Django docs:
+                # https://docs.djangoproject.com/en/dev/ref/contrib/sites/#getting-the-current-domain-for-full-urls
+                "item": f'{protocol}://{domain}{page.url}',
             })
         item_list.append({
                 "@type": "ListItem",
                 "position": len(self.parent_pages) + 1,
                 "name": self.title,
-                "item": self.url
+                "item": f'{protocol}://{domain}{self.url}',
             })
         return {
             "@context": "https://schema.org",
